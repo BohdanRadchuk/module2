a146a578f81daf55da04c2f62aa15e6412123813
BohdanRadchuk <shpionuse@gmail.com>
2017-09-07 11:38:06 +0300

    Name

--- src/module2dz.java
+++ src/module2dz.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class module2dz {
     public static void main(String[] args) {
-        
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = in.nextLine();
+        System.out.println("Name:" + name);
+
     }
 }
