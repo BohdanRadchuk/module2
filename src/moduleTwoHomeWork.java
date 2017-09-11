fbc9b2f5b95e7b36187ad89ac523f8027839cf89
BohdanRadchuk <shpionuse@gmail.com>
2017-09-11 09:40:35 +0300

    Final code. renamed

--- src/moduleTwoHomeWork.java
+++ src/moduleTwoHomeWork.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 
-public class moduleTwoHomeWork {
+public class ModuleTwoHomeWork {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
@@ -9,19 +9,16 @@ public class moduleTwoHomeWork {
         String name = in.nextLine();
         System.out.println("What city do you live in?");
         String city = in.nextLine();
-
         System.out.println("How old are you?");
         int age = in.nextInt();
-        in.nextLine();
         System.out.println("What is your hobby?");
-
+        in.nextLine();
         String hobby = in.nextLine();
         System.out.println("-----------------");
         System.out.println("Name: " + name);
         System.out.println("City: " + city);
         System.out.println("Age: " + age);
         System.out.println("Hobby: " + hobby);
-
         System.out.println("-----------------");
         System.out.println("Человек по имени " + name + " живет в городе " + city + ".\n" + "Этому человеку " + age + " лет и он любит заниматься " + hobby + ".");
         System.out.println("-----------------\n" + name + " - имя\n" + city + " - город\n" + age + " - возраст\n" + hobby + " - хобби \n-----------------");
